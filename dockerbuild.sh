4b43d788d8e58da9ea67cc07981ae91f2a281556
d032408 <bernd.kuespert@sap.com>
2021-07-26 20:49:40 +0200

    remove dockerbuild script. use the pipeline instead

--- dockerbuild.sh
+++
@@ -1,6 +0,0 @@
-version="v0.1.12"
-image = "redfish-exporter"
-
-docker login keppel.eu-de-1.cloud.sap
-docker build . -t keppel.eu-de-1.cloud.sap/ccloud/${image}:${version}
-docker push keppel.eu-de-1.cloud.sap/ccloud/${image}:${version}

deafbe6e6771e441a823e56ef1f1f268bf928515
d032408 <bernd.kuespert@sap.com>
2023-10-18 14:00:11 +0200

    rename the label 'name' to 'device_name'

--- collectors/firmware_collector.py
+++ collectors/firmware_collector.py
@@ -29,27 +29,27 @@ class FirmwareCollector(object):
             fw_member_url = fw_member['@odata.id']
             # only look at entries on a Dell server if the device is markedd as installed
             if (search(".*Dell.*", self.col.manufacturer) and ("Installed" in fw_member_url)) or not search(".*Dell.*", self.col.manufacturer):
-                server_response = self.col.connect_server(fw_member_url)
-                if not server_response:
+                fw_item = self.col.connect_server(fw_member_url)
+                if not fw_item:
                     continue
 
-                name = server_response['Name'].split(",", 1)[0]
-                current_labels = {"name": name}
+                device_name = fw_item['Name'].split(",", 1)[0]
+                current_labels = {"device_name": device_name}
 
                 if self.col.manufacturer == 'Lenovo':
                     # Lenovo has always Firmware: in front of the names, let's remove it
-                    name = server_response['Name'].replace('Firmware:','')
-                    current_labels.update({"name": name})
+                    device_name = fw_item['Name'].replace('Firmware:','')
+                    current_labels.update({"device_name": device_name})
                     # we need an additional label to distinguish the metrics because
                     # the device ID is not in the name in case of Lenovo
-                    if "Id" in server_response:
-                        current_labels.update({"id": server_response['Id']})
+                    if "Id" in fw_item:
+                        current_labels.update({"id": fw_item['Id']})
 
-                if "Manufacturer" in server_response:
-                    current_labels.update({"device_manufacturer": server_response['Manufacturer']})
+                if "Manufacturer" in fw_item:
+                    current_labels.update({"device_manufacturer": fw_item['Manufacturer']})
 
-                if "Version" in server_response:
-                    version = server_response['Version']
+                if "Version" in fw_item:
+                    version = fw_item['Version']
                     if version != "N/A" and version != None:
                         current_labels.update({"version": version})
                         current_labels.update(self.col.labels)

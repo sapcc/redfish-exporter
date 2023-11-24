6dafe4af5338c90158ffad16efcdce6e6a2a31f6
d032408 <bernd.kuespert@sap.com>
2023-11-24 08:49:03 +0100

    some more debugging of cert verification

--- collectors/certificate_collector.py
+++ collectors/certificate_collector.py
@@ -43,24 +43,8 @@ class CertificateCollector(object):
         context = ssl.create_default_context()
         context.check_hostname = False
         context.load_default_certs()
-        root_certificates = context.get_ca_certs()
-
-        if root_certificates:
-            for cert in root_certificates:
-                issuer = dict(x[0] for x in cert['issuer'])
-                logging.info(f"Target {self.target}: issuer name: {issuer.get('commonName')}")
-        else:
-            logging.info("No Root CA Certs found!")
-
-        context.load_verify_locations(capath='/usr/local/share/ca-certificates')
-        root_certificates = context.get_ca_certs()
-
-        if root_certificates:
-            for cert in root_certificates:
-                issuer = dict(x[0] for x in cert['issuer'])
-                logging.info(f"Target {self.target}: issuer name: {issuer.get('commonName')}")
-        else:
-            logging.info("No Root CA Certs found!")
+        default_path = ssl.get_default_verify_paths()
+        logging.debug(f"Target {self.target}: Default cert path: {default_path}")
             
         context.load_verify_locations(cafile='/usr/local/share/ca-certificates/SAPNetCA_G2.crt')
         root_certificates = context.get_ca_certs()
@@ -72,7 +56,7 @@ class CertificateCollector(object):
         else:
             logging.info("No Root CA Certs found!")
             
-        # context.verify_mode = ssl.CERT_NONE
+        context.verify_mode = ssl.CERT_REQUIRED
         cert_days_left = 0
         cert_valid = 0
         cert_has_right_hostname = 0
@@ -89,6 +73,32 @@ class CertificateCollector(object):
             conn = context.wrap_socket(sock, server_hostname=self.host)
             conn.connect((self.host, self.port))
             cert = conn.getpeercert()
+
+        except ssl.SSLCertVerificationError as e:
+
+            logging.debug(f"Target {self.target}: Certificate Validation Error!")
+            logging.debug(f"Target {self.target}: Reason: {e.reason}")
+            logging.debug(f"Target {self.target}: Verify Message: {e.verify_message}")
+
+            if e.verify_message == 'self-signed certificate':
+                cert_selfsigned = 1
+                current_labels.update({"issuer": "self-signed"})
+
+            elif e.verify_message == 'unable to get issuer certificate':
+                cert = e.cert
+
+            else:
+                return
+
+        except TimeoutError:
+            logging.debug(f"Target {self.target}: Timeout occured!")
+            return
+        
+        finally:
+            conn.close()
+            sock.close()
+
+        if cert:
             cert_expiry_date = datetime.datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z') if 'notAfter' in cert else datetime.datetime.now()
             cert_days_left = (cert_expiry_date - datetime.datetime.now()).days
             issuer = dict(x[0] for x in cert['issuer'])
@@ -109,22 +119,6 @@ class CertificateCollector(object):
             if cert_days_left > 0 and cert_has_right_hostname:
                 cert_valid = 1
 
-
-        except ssl.SSLCertVerificationError as e:
-            if e.verify_message == 'self-signed certificate':
-                cert_selfsigned = 1
-                current_labels.update({"issuer": "self-signed"})
-            else:
-                logging.debug(f"Target {self.target}: Certificate Validation Error: {e}")
-                logging.debug(f"Target {self.target}: Verify Message: {e.verify_message}")
-                return
-        except TimeoutError:
-            logging.debug(f"Target {self.target}: Timeout occured!")
-            return
-        
-        finally:
-            conn.close()
-            sock.close()
 
         current_labels.update(self.labels)
 

86da4890341feea54cf2fc04077bff2ff7bedd3c
d032408 <bernd.kuespert@sap.com>
2024-09-27 16:06:15 +0200

    get the target and job vars from the params in the on_get function and don't define them als self vars

--- handler.py
+++ handler.py
@@ -45,72 +45,71 @@ class MetricsHandler:
     def __init__(self, config, metrics_type):
         self._config = config
         self.metrics_type = metrics_type
-        self.target = None
-        self._job = None
-        self.host = None
 
     def on_get(self, req, resp):
         """
         Define the GET method for the API.
         """
-        self.target = req.get_param("target")
-        if not self.target:
+        target = req.get_param("target")
+        if not target:
             logging.error("No target parameter provided!")
             raise falcon.HTTPMissingParam("target")
 
-        logging.debug("Received Target: %s", self.target)
+        logging.debug("Received Target: %s", target)
 
-        self._job = req.get_param("job")
-        if not self._job:
-            logging.error("Target %s: No job provided!", self.target)
+        job = req.get_param("job")
+        if not job:
+            logging.error("Target %s: No job provided!", target)
             raise falcon.HTTPMissingParam("job")
 
-        logging.debug("Received Job: %s", self._job)
+        logging.debug("Received Job: %s", job)
 
         ip_re = re.compile(
-            r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+            r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}"
+            r"([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
         )
 
         resp.set_header("Content-Type", CONTENT_TYPE_LATEST)
 
-        if ip_re.match(self.target):
-            logging.debug("Target %s: Target is an IP Address.", self.target)
+        host = None
+        if ip_re.match(target):
+            logging.debug("Target %s: Target is an IP Address.", target)
             try:
-                host = socket.gethostbyaddr(self.target)[0]
-                if host:
-                    self.host = host
+                host = socket.gethostbyaddr(target)[0]
             except socket.herror as err:
-                msg = f"Target {self.target}: Reverse DNS lookup failed: {err}"
+                msg = f"Target {target}: Reverse DNS lookup failed: {err}"
                 logging.error(msg)
                 raise falcon.HTTPInvalidParam(msg, "target")
         else:
-            logging.debug("Target %s: Target is a hostname.", self.target)
-            self.host = self.target
+            logging.debug("Target %s: Target is a hostname.", target)
+            host = target
             try:
-                target = socket.gethostbyname(self.host)
-                if target:
-                    self.target = target
+                target = socket.gethostbyname(host)
             except socket.gaierror as err:
-                msg = f"Target {self.target}: DNS lookup failed: {err}"
+                msg = f"Target {target}: DNS lookup failed: {err}"
                 logging.error(msg)
                 raise falcon.HTTPInvalidParam(msg, "target")
 
-        usr_env_var = self._job.replace("-", "_").upper() + "_USERNAME"
-        pwd_env_var = self._job.replace("-", "_").upper() + "_PASSWORD"
+        usr_env_var = job.replace("-", "_").upper() + "_USERNAME"
+        pwd_env_var = job.replace("-", "_").upper() + "_PASSWORD"
         usr = os.getenv(usr_env_var, self._config.get("username"))
         pwd = os.getenv(pwd_env_var, self._config.get("password"))
 
         if not usr or not pwd:
-            msg = f"Target {self.target}: Unknown job provided or no user/password found in environment and config file: {self._job}"
+            msg = (
+                f"Target {target}: "
+                "Unknown job provided or "
+                f"no user/password found in environment and config file: {job}"
+            )
             logging.error(msg)
             raise falcon.HTTPInvalidParam(msg, "job")
 
-        logging.debug("Target %s: Using user %s", self.target, usr)
+        logging.debug("Target %s: Using user %s", target, usr)
 
         with RedfishMetricsCollector(
             self._config,
-            target = self.target,
-            host = self.host,
+            target = target,
+            host = host,
             usr = usr,
             pwd = pwd,
             metrics_type = self.metrics_type
@@ -126,5 +125,5 @@ class MetricsHandler:
 
             except Exception as err:
                 message = f"Exception: {traceback.format_exc()}"
-                logging.error("Target %s: %s", self.target, message)
+                logging.error("Target %s: %s", target, message)
                 raise falcon.HTTPBadRequest("Bad Request", message)

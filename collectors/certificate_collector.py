7c94f2494baf3e8e7c0f9f2aacba937908acb145
d032408 <bernd.kuespert@sap.com>
2024-11-11 11:24:27 +0100

    fix the certificate metrics

--- collectors/certificate_collector.py
+++ collectors/certificate_collector.py
@@ -16,33 +16,29 @@ class CertificateCollector:
         self.labels = labels
         self.port = 443
 
-        self.cert_metrics = {
-            "isvalid": GaugeMetricFamily(
-                "redfish_certificate_isvalid",
-                "Redfish Server Monitoring certificate is valid",
-                labels=self.labels,
-            ),
-            "valid_hostname": GaugeMetricFamily(
-                "redfish_certificate_valid_hostname",
-                "Redfish Server Monitoring certificate has valid hostname",
-                labels=self.labels,
-            ),
-            "valid_days": GaugeMetricFamily(
-                "redfish_certificate_valid_days",
-                "Redfish Server Monitoring certificate valid for days",
-                labels=self.labels,
-            ),
-            "selfsigned": GaugeMetricFamily(
-                "redfish_certificate_selfsigned",
-                "Redfish Server Monitoring certificate is self-signed",
-                labels=self.labels,
-            ),
-        }
+        self.cert_metrics_isvalid = GaugeMetricFamily(
+                name = "redfish_certificate_isvalid",
+                documentation = "Redfish Server Monitoring certificate is valid",
+                labels = self.labels,
+        )
 
-    def reset_metrics(self):
-        """Resets the certificate metrics."""
-        for metric in self.cert_metrics.values():
-            metric.samples.clear()
+        self.cert_metrics_valid_hostname = GaugeMetricFamily(
+                name = "redfish_certificate_valid_hostname",
+                documentation = "Redfish Server Monitoring certificate has valid hostname",
+                labels = self.labels,
+        )
+
+        self.cert_metrics_valid_days = GaugeMetricFamily(
+                name = "redfish_certificate_valid_days",
+                documentation = "Redfish Server Monitoring certificate valid for days",
+                labels = self.labels,
+        )
+
+        self.cert_metrics_selfsigned = GaugeMetricFamily(
+                name = "redfish_certificate_selfsigned",
+                documentation = "Redfish Server Monitoring certificate is self-signed",
+                labels = self.labels,
+        )
 
     def collect(self):
         '''Collect Certificate data'''
@@ -130,25 +126,25 @@ class CertificateCollector:
 
         current_labels.update(self.labels)
 
-        self.cert_metrics['isvalid'].add_sample(
+        self.cert_metrics_isvalid.add_sample(
             "redfish_certificate_isvalid",
             value = cert_valid,
             labels = current_labels,
         )
 
-        self.cert_metrics['valid_hostname'].add_sample(
+        self.cert_metrics_valid_hostname.add_sample(
             "redfish_certificate_valid_hostname",
             value = cert_has_right_hostname,
             labels = current_labels,
         )
 
-        self.cert_metrics['valid_days'].add_sample(
+        self.cert_metrics_valid_days.add_sample(
             "redfish_certificate_valid_days",
             value = cert_days_left,
             labels = current_labels,
         )
 
-        self.cert_metrics['selfsigned'].add_sample(
+        self.cert_metrics_selfsigned.add_sample(
             "redfish_certificate_selfsigned",
             value = cert_selfsigned,
             labels = current_labels,
